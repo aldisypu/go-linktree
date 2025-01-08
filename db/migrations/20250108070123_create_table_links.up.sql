f3a7f2eeea414b0e2c71340e9c8370292962a0dd
aldisypu <aldisypu@gmail.com>
2025-01-08 14:44:30 +0700

    fix: correct  migration script for links table

--- db/migrations/20250108070123_create_table_links.up.sql
+++ db/migrations/20250108070123_create_table_links.up.sql
@@ -3,9 +3,9 @@ create table links
     id varchar(100) not null,
     title varchar(100) not null,
     url varchar(2048) not null,
-    username varchar(100) not null
+    username varchar(100) not null,
     created_at bigint not null,
     updated_at bigint not null,
-    primary key (id)
+    primary key (id),
     foreign key fk_links_username (username) references users (username)
 ) engine = InnoDB;
