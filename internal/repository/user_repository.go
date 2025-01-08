611d9344f793fcdff0aeba0e654ad6543f327766
aldisypu <aldisypu@gmail.com>
2025-01-08 17:53:35 +0700

    feat: add repository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,29 @@
+package repository
+
+import (
+	"go-linktree/internal/entity"
+
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	Repository[entity.User]
+	Log *logrus.Logger
+}
+
+func NewUserRepository(log *logrus.Logger) *UserRepository {
+	return &UserRepository{
+		Log: log,
+	}
+}
+
+func (r *UserRepository) FindByToken(db *gorm.DB, user *entity.User, token string) error {
+	return db.Where("token = ?", token).First(user).Error
+}
+
+func (r *UserRepository) CountByUsername(db *gorm.DB, username string) (int64, error) {
+	var total int64
+	err := db.Model(&entity.User{}).Where("username = ?", username).Count(&total).Error
+	return total, err
+}
