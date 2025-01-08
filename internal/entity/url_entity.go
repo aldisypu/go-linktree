543e1f4d8f5afb914c578275bb278fae03ffb0e1
aldisypu <aldisypu@gmail.com>
2025-01-08 15:21:59 +0700

    feat: add entity

---
+++ internal/entity/url_entity.go
@@ -0,0 +1,15 @@
+package entitiy
+
+type Url struct {
+	ID        string `gorm:"column:id;primaryKey"`
+	Title     string `gorm:"column:title"`
+	Url       string `gorm:"column:url"`
+	Username  string `gorm:"column:username"`
+	CreatedAt int64  `gorm:"column:created_at;autoCreateTime:milli"`
+	UpdatedAt int64  `gorm:"column:updated_at;autoCreateTime:milli;autoUpdateTime:milli"`
+	User      User   `gorm:"foreignKey:username;references:username"`
+}
+
+func (r *Url) TableName() string {
+	return "urls"
+}
