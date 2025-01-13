6edffbf58078e508db41b5ee5b0fbefcaf77bd90
aldisypu <aldisypu@gmail.com>
2025-01-13 23:58:29 +0700

    feat: add bootsrap config

--- internal/config/app.go
+++ internal/config/app.go
@@ -1,6 +1,12 @@
 package config
 
 import (
+	"go-linktree/internal/delivery/http"
+	"go-linktree/internal/delivery/http/middleware"
+	"go-linktree/internal/delivery/http/route"
+	"go-linktree/internal/repository"
+	"go-linktree/internal/usecase"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
@@ -14,4 +20,25 @@ type BootstrapConfig struct {
 	Log      *logrus.Logger
 	Validate *validator.Validate
 	Config   *viper.Viper
+}
+
+func Bootstrap(config *BootstrapConfig) {
+	userRepository := repository.NewUserRepository(config.Log)
+	urlRepository := repository.NewUrlRepository(config.Log)
+
+	userUseCase := usecase.NewUserUseCase(config.DB, config.Log, config.Validate, userRepository)
+	urlUseCase := usecase.NewUrlUseCase(config.DB, config.Log, config.Validate, urlRepository)
+
+	userController := http.NewUserController(userUseCase, config.Log)
+	urlController := http.NewUrlController(urlUseCase, config.Log)
+
+	authMiddleware := middleware.NewAuth(userUseCase)
+
+	routeConfig := route.RouteConfig{
+		App:            config.App,
+		UserController: userController,
+		UrlController:  urlController,
+		AuthMiddleware: authMiddleware,
+	}
+	routeConfig.Setup()
 }
